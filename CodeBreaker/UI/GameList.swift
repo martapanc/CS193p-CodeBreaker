261da390ec36efcc45116ed90ea6754fc42f6cde
martapanc <marta.panc@gmail.com>
2026-05-06 14:25:56 +0200

    Add GameList

---
+++ CodeBreaker/UI/GameList.swift
@@ -0,0 +1,18 @@
+//
+//  GameList.swift
+//  CodeBreaker
+//
+//  Created by Marta Pancaldi on 6/5/26.
+//
+
+import SwiftUI
+
+struct GameList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameList()
+}
